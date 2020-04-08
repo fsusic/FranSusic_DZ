f519b559e54a4a07697dd1ed2dd31aa8b1579997
Fran Susic <fran.susic2@gmail.com>
2020-04-08 14:41:06 +0200

    Fran Susic DZ

---
+++ SpaDomacaZadaca01/Source.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+
+int main()
+{
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!", sf::Style::Default, settings);
+	window.setFramerateLimit(60);
+	Cvijet cvijet(&window);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		sf::Clock frameClock;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color(135,205,235));
+
+		sf::Time elapsed = frameClock.restart();
+
+		cvijet.draw();
+
+		window.display();
+	}
+
+	return 0;
+}
